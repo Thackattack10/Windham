ca3aa66ec6cc0482b4f6803efc45df18c8093324
Thackattack10 <50476366+Thackattack10@users.noreply.github.com>
2025-07-29 14:26:13 -0500

    Update projections.py

--- projections.py
+++ projections.py
@@ -1,2 +1,6 @@
 def project_golf_points(row):
-    return row.get('FPPG', 0) + row.get('SG_Putting', 0) * 2 + row.get('Salary', 0) * 0.0005
+    return (
+        row.get('FPPG', 0) +
+        row.get('SG_Putting', 0) * 2 +
+        row.get('Salary', 0) * 0.0005
+    )
