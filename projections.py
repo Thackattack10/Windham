1e40ea691a7302d84dbb3ade024958599e98dddd
Thackattack10 <50476366+Thackattack10@users.noreply.github.com>
2025-08-01 14:23:20 -0500

    Update projections.py

--- projections.py
+++ projections.py
@@ -1,8 +1,11 @@
+ott_weight = st.slider("Weight: SG OTT", 0.0, 1.0, 0.25)
+app_weight = st.slider("Weight: SG APP", 0.0, 1.0, 0.3)
+putt_weight = st.slider("Weight: SG Putting", 0.0, 1.0, 0.2)
+
 def project_golf_points(row):
-    # Adjust weights as needed
     return (
-        0.5 * row['FPPG'] +
-        2.0 * row['SG_Putting'] +
-        2.5 * row['SG_APP'] +
-        1.8 * row['SG_OTT']
+        row['FPPG'] +
+        putt_weight * row['SG_Putting'] +
+        app_weight * row['SG_APP'] +
+        ott_weight * row['SG_OTT']
     )
